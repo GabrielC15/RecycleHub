e79721fdb1bdc569fb0b1adc1ce0548bc29dffc7
Gabriel <104772894+GabrielC15@users.noreply.github.com>
2025-03-15 15:49:57 +1100

    attempt to retrieve data from backend

--- FrontEnd/components/Listing.vue
+++ FrontEnd/components/Listing.vue
@@ -2,18 +2,18 @@
   <v-container>
     <v-layout>
       <v-card hover>
+        <v-img :src="{ image }"></v-img>
         <v-card-title>
           <h2>{{ title }}</h2>
         </v-card-title>
 
         <v-card-text>
-          line 1<br />
-          line 2<br />
+          {{ description }}
         </v-card-text>
 
         <v-card-actions>
-          <v-btn color="success">Click #1</v-btn>
-          <v-btn flat color="primary">Click #2</v-btn>
+          <v-btn color="success">{{ action }}</v-btn>
+          <v-btn flat color="primary">{{ material }}</v-btn>
           <v-spacer></v-spacer>
           <v-btn icon><v-icon>bookmark</v-icon></v-btn>
         </v-card-actions>
@@ -25,5 +25,11 @@
 <script setup>
 defineProps({
   title: String,
+  description: String,
+  location: String,
+  action: String,
+  material: String,
+  image: String,
+  createdAt: Date,
 });
 </script>
--- FrontEnd/pages/index.vue
+++ FrontEnd/pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <v-row justify="center" dense>
-    <v-col cols="12" md="6">
-      <Listing title="Product1"></Listing>
-    </v-col>
-    <v-col cols="12" md="6">
-      <Listing title="Product2"></Listing>
-    </v-col>
+    <v-row justify="center" dense>
+      <v-col cols="12" md="6" v-for="listing in listings" :key="listing.id">
+        <Listing
+          :title="listing.title"
+          :description="listing.description"
+          :location="listing.location"
+          :action="listing.action"
+          :material="listing.material"
+          :image="listing.image"
+          :created-at="listing.created_at"
+        />
+      </v-col>
+    </v-row>
   </v-row>
 </template>
 
@@ -14,7 +21,7 @@ const listings = ref([]);
 
 onMounted(async () => {
   try {
-    const response = await fetch("http://localhost:3000");
+    const response = await fetch("http://localhost:5000");
     listings.value = response.json();
   } catch (error) {
     console.error(error);
